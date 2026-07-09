b2550ac857ede63c71ac7826531cadd2895479a7
Podlipny Vadim <119167052+KyerrreN@users.noreply.github.com>
2026-07-09 14:51:50 +0200

    feat: add projectapikey markers to all queries that use it

--- Backend/Read/Telemetry.Read.API/Features/GetTopEvents/GetTopEventsQuery.cs
+++ Backend/Read/Telemetry.Read.API/Features/GetTopEvents/GetTopEventsQuery.cs
@@ -4,7 +4,7 @@ using Telemetry.Read.Domain.Abstractions.Markers;
 namespace Telemetry.Read.API.Features.GetTopEvents;
 
 public record GetTopEventsQuery(string ProjectApiKey, DatePeriodEnum Period = DatePeriodEnum.Last7Days, int Limit = 10)
-    : IQuery<GetTopEventsResponse>, ICachableQuery
+    : IQuery<GetTopEventsResponse>, ICachableQuery, IProjectApiQuery
 {
     public string CacheKey => $"top-events:{ProjectApiKey}_period:{Period}_limit:{Limit}";
 
